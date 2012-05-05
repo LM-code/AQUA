e010792809f587f529a28bb168b3ca2b52cecb60
LM_code <webmaster@super-annonce.fr>
2012-05-05 21:11:56 +0200

    Create folder Aqua
    Use of autotools

---
+++ Aqua/config.h
@@ -0,0 +1,29 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.in by autoheader.  */
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "Aqua"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "webmaster@super-annonce.fr"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "Aqua"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "Aqua 0.9.0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "aqua"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.9.0.1"
+
+/* Version number of package */
+#define VERSION "0.9.0.1"
